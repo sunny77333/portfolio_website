2a5c5277739da521d236950994f2faffda258cf6
Sunny <128038119+sunny77333@users.noreply.github.com>
2024-12-25 20:41:25 +1300

    project setup inital design

---
+++ src/sections/Portfolio.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Portfolio() {
+  return (
+    <div>
+      <h1>My Portfolio</h1>
+      <p>Here are some of my projects:</p>
+      <ul>
+        <li>Project 1 - Description</li>
+        <li>Project 2 - Description</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Portfolio;
